a091ab9c3dc1d4aac3ad3131b9391b13168f5c67
Danijel Stanko <stankodanijel@gmail.com>
2025-03-24 12:44:26 +0100

    basic routing and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import ProductList from "./pages/ProductList/ProductList";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
